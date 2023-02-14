0145e996d7a28b940593209f68e1cd72206f9c43
Ankit Banerjee <ankitbanerjee0211@gmail.com>
2023-02-14 20:34:33 +0530

    Final Update: HDFC Bank Added

--- src/HDFCAccount.java
+++ src/HDFCAccount.java
@@ -1,2 +1,87 @@
-public class HDFCAccount {
+import java.util.UUID;
+
+public class HDFCAccount implements BankInterface{
+
+    private String name;
+    private String accountNo; // to use UUID for random unique name
+    private double balance;
+    private String password;
+    private double rateOfInterest;
+
+    // alt + ins => generate getters and setters, constructors
+    public HDFCAccount(String name, int balance, String password){
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+
+        this.rateOfInterest = 7.8;
+        this.accountNo = String.valueOf(UUID.randomUUID());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        accountNo = accountNo;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    public void setRateOfInterest(double rateOfInterest) {
+        this.rateOfInterest = rateOfInterest;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public String depositMoney(int amount) {
+        this.balance += amount;
+        return "Amount of " + amount + " has been credited to your account. Current available balance " + getBalance() + ".";
+    }
+
+    @Override
+    public String withdraw(int amount, String enteredPassword) {
+        if(password.equals(enteredPassword)){
+            if(balance > amount){
+                this.balance -= amount;
+                return "Amount of " + amount + " has been debited from your account. Current available balance " + getBalance() + ".";
+            } else{
+                return "Insufficient Account Balance";
+            }
+        } else {
+            return "Password Mismatch";
+        }
+
+    }
+
+    @Override
+    public double calculateInterest(int time) {
+        return (balance*rateOfInterest*time)/100.0;
+    }
 }
